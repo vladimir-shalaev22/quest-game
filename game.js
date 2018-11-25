e1016af5b7f4bd2fbd67018b489975a8c8d6e102
Vladimir Shalaev <vladimir-shalaev22@yandex.ru>
2018-11-25 15:55:43 +0700

    Еле работающая версия

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+var __GAME__ = {};
+
 (function (game) {
   /* Все экраны игры (её стадии) */
   var SCREEN_INTRO = 'SCREEN_INTRO'
@@ -32,11 +34,14 @@
   /* Статичные данные игры */
   var staticData = {
     cells: [
-      {
-        type: CELL_TYPE_START,
-        position: [30, 500],
-        screen: 0
-      }
+      {type: CELL_TYPE_START, position: [52, 548], next: 1},
+      {type: CELL_TYPE_SIMPLE, position: [119, 482], next: 2},
+      {type: CELL_TYPE_SIMPLE, position: [171, 430], next: 3},
+      {type: CELL_TYPE_SIMPLE, position: [230, 384], next: 4},
+      {type: CELL_TYPE_SIMPLE, position: [368, 374], next: 5},
+      {type: CELL_TYPE_SIMPLE, position: [436, 376], next: 6},
+      {type: CELL_TYPE_SIMPLE, position: [499, 409], next: 7},
+      {type: CELL_TYPE_FINISH, position: [567, 427], next: 0}
     ]
   }
 
@@ -48,9 +53,13 @@
     movement: {
       startCell: 0,
       endCell: 0,
-      progress: 0
+      progress: 0,
+      player: 0
     }
   }
+
+  /* Подписчики на изменения стейта */
+  var listeners = []
 
   /* Определяет закончил ли игрок игру */
   function isFinished(player) {
@@ -58,13 +67,13 @@
   }
 
   /* Вычисляет следующий экран игры, в случае если мы в SCREEN_GAMEPLAY */
-  function processGameplayScreen(state, action) {
+  function processGameplayScreen(state, action, movement) {
     switch (action.type) {
       case GAME_NEXT_FRAME:
-        if (state.movement.progress < 1) {
+        if (movement.progress < 1) {
           return SCREEN_GAMEPLAY
         } else {
-          switch (staticData.cells[state.movement.endCell].type) {
+          switch (staticData.cells[movement.endCell].type) {
             case CELL_TYPE_SIMPLE: return SCREEN_DICE
             case CELL_TYPE_QUESTION: return SCREEN_QUESTION
             case CELL_TYPE_RETURN: return SCREEN_GAMEPLAY
@@ -81,7 +90,7 @@
   }
 
   /* Вычисляет следующий экран игры */
-  function processScreen(state, action) {
+  function processScreen(state, action, movement) {
     switch (state.screen) {
       case SCREEN_INTRO:
         return action.type === GAME_START ? SCREEN_PREPARE : SCREEN_INTRO
@@ -91,7 +100,7 @@
       case SCREEN_DICE:
         return action.type === GAME_MAKE_TURN ? SCREEN_GAMEPLAY : SCREEN_DICE
       case SCREEN_GAMEPLAY:
-        return processGameplayScreen(state, action)
+        return processGameplayScreen(state, action, movement)
       case SCREEN_PAUSE:
         if (action.type === GAME_RESUME) {
           return SCREEN_GAMEPLAY
@@ -142,7 +151,7 @@
   }
 
   /* Вычисляет состояние игроков */
-  function processPlayers(state, action) {
+  function processPlayers(state, action, movement) {
     var endCellType = staticData.cells[state.movement.endCell].type
     var players = state.players.slice()
     switch (action.type) {
@@ -152,10 +161,10 @@
       case GAME_QUIT:
         return []
       case GAME_NEXT_FRAME:
-        if (state.movement.progress < 1) {
-          return state.players
-        } else {
+        if (movement.progress === 1 && state.screen === SCREEN_GAMEPLAY) {
           return updatePlayers(state, endCellType)
+        } else {
+          return state.players
         }
       case GAME_MAKE_ANSWER:
         players[state.currentPlayer] = Object.assign({}, players[state.currentPlayer], {
@@ -191,19 +200,19 @@
   }
 
   /* Вычисляет текущего игрока */
-  function processCurrent(state, action, players) {
-    var endCellType = staticData.cells[state.movement.endCell].type
+  function processCurrent(state, action, players, movement) {
+    var endCellType = staticData.cells[movement.endCell].type
     var isGameOver = players.every(isFinished)
     var next = state.currentPlayer + 1
     var max = players.length
     switch (action.type) {
       case GAME_NEXT_FRAME:
-        if (state.movement.progress < 0) {
+        if (movement.progress < 1) {
           return {
             players: players,
             current: state.currentPlayer
           }
-        } else {
+        } else if (state.screen === SCREEN_GAMEPLAY) {
           switch (endCellType) {
             case CELL_TYPE_FINISH:
             case CELL_TYPE_SIMPLE:
@@ -213,6 +222,8 @@
             default:
               return {players: players, current: state.currentPlayer}
           }
+        } else {
+          return {players: players, current: state.currentPlayer}
         }
       case GAME_MAKE_ANSWER:
         return !isGameOver ? findNext(players, next < max ? next : 0, max)
@@ -233,6 +244,7 @@
         })
       } else if (endCellType === CELL_TYPE_RETURN) {
         return {
+          player: state.currentPlayer,
           startCell: state.movement.endCell,
           endCell: staticData.cells[state.movement.endCell].target,
           progress: 0
@@ -283,16 +295,167 @@
 
   /* Функция вычисляющая следующие состояние */
   function nextState(state, action) {
-    var updatedPlayers = processPlayers(state, action)
-    var next = processCurrent(state, action, updatedPlayers)
+    var movement = processMovement(state, action)
+    var updatedPlayers = processPlayers(state, action, movement)
+    var next = processCurrent(state, action, updatedPlayers, movement)
     return {
-      screen: processScreen(state, action),
+      screen: processScreen(state, action, movement),
       players: next.players,
       currentPlayer: next.current,
-      movement: processMovement(state, action)
+      movement: movement
     }
   }
 
-  /* Выводим начальное состояние */
-  console.log(state)
-})(window)
+  function dispatch(action) {
+    state = nextState(state, action)
+    listeners.forEach(function (listener) {
+      if (typeof listener === 'function') {
+        listener(state)
+      }
+    })
+    console.log(state)
+  }
+
+  function subscribe(callback) {
+    listeners.push(callback)
+    callback(state)
+  }
+
+  function addPlayer(player) {
+    dispatch({type: GAME_ADD_PLAYER, player: Object.assign({
+      isFinished: false,
+      isRight: false,
+      toSkip: false,
+      position: 0,
+      score: 0
+    }, player)})
+  }
+
+  function makeTurn() {
+    dispatch({type: GAME_MAKE_TURN, dice: Date.now() % 6 + 1})
+  }
+
+  Object.assign(game, {
+    state: state,
+    dispatch: dispatch,
+    subscribe: subscribe,
+    addPlayer: addPlayer,
+    makeTurn: makeTurn,
+    staticData: staticData
+  })
+})(__GAME__);
+
+/* Работа с визуальной частью */
+(function initVisual(game) {
+  var gameView = {
+    canvas: document.getElementById('canvas'),
+    screens: {
+      intro: {
+        root: document.getElementById('intro-screen'),
+        start: document.getElementById('start-game')
+      },
+      prepare: {
+        root: document.getElementById('prepare-screen'),
+        start: document.getElementById('start-game-2'),
+        list: document.getElementById('players-list'),
+        name: document.getElementById('player-name'),
+        add: document.getElementById('add-player')
+      },
+      dice: {
+        root: document.getElementById('dice-screen'),
+        cta: document.getElementById('dice-cta'),
+        result: document.getElementById('dice-result'),
+        action: document.getElementById('make-turn')
+      }
+    }
+  }, ctx = gameView.canvas.getContext('2d')
+
+  function updateScreen(name, isOpen) {
+    gameView.screens[name].root
+      .setAttribute('class', isOpen ? 'game__screen game__screen_active' : 'game__screen')
+  }
+
+  function makePlayersList(state) {
+    if (state.players.length > 0) {
+      return state.players.map(function playerName(player) {
+        return player.name
+      }).join(', ') + ', поздравляем, вы в игре!'
+    } else {
+      return 'Как вас зовут? Или придумаем ник?'
+    }
+  }
+
+  function calcMovementPosition(state) {
+    var start = game.staticData.cells[state.movement.startCell].position
+    var end = game.staticData.cells[state.movement.endCell].position
+    return [
+      start[0] + (end[0] - start[0]) * state.movement.progress,
+      start[1] + (end[1] - start[1]) * state.movement.progress
+    ]
+  }
+
+  function renderField(state) {
+    ctx.clearRect(0, 0, 800, 600)
+    game.staticData.cells.forEach(function renderCell(cell) {
+      ctx.fillStyle = 'rgba(46, 42, 94, 1)'
+      ctx.fillRect(cell.position[0] - 20, cell.position[1] - 20, 40, 40)
+    })
+    state.players.forEach(function renderPlayer(player, index) {
+      var cell = game.staticData.cells[player.position]
+      var position = calcMovementPosition(state)
+      if (index !== state.currentPlayer && state.movement.progress < 1) {
+        ctx.fillStyle = 'rgba(0, 150, 136, 1)'
+        ctx.fillRect(cell.position[0] - 15, cell.position[1] - 15, 30, 30)
+      } else {
+        ctx.fillStyle = 'rgba(0, 150, 136, 1)'
+        ctx.fillRect(position[0] - 15, position[1] - 15, 30, 30)
+      }
+    })
+  }
+
+  function updateView(state) {
+    updateScreen('intro', state.screen === 'SCREEN_INTRO')
+    updateScreen('prepare', state.screen === 'SCREEN_PREPARE')
+    updateScreen('dice', state.screen === 'SCREEN_DICE')
+    gameView.screens.prepare.list.innerText = makePlayersList(state)
+    renderField(state)
+  }
+
+  gameView.screens.intro.start.addEventListener('click', function (event) {
+    event.preventDefault()
+    game.dispatch({type: 'GAME_START'})
+  })
+
+  gameView.screens.prepare.add.addEventListener('click', function (event) {
+    var name = gameView.screens.prepare.name.value
+    event.preventDefault()
+    if (name !== '') {
+      gameView.screens.prepare.name.value = ''
+      game.addPlayer({name: name})
+    }
+  })
+
+  gameView.screens.prepare.start.addEventListener('click', function (event) {
+    event.preventDefault()
+    game.dispatch({type: 'GAME_START'})
+  })
+
+  gameView.screens.dice.action.addEventListener('click', function (event) {
+    var dice = Date.now() % 6 + 1
+    event.preventDefault()
+    gameView.screens.dice.result.innerText = 'У вас выпало: ' + dice
+    setTimeout(function () {
+      game.dispatch({type: 'GAME_MAKE_TURN', dice: dice})
+      gameView.screens.dice.result.innerText = 'Бросьте игральную кость на удачу!'
+    }, 500)
+  })
+
+  game.subscribe(updateView)
+
+  function animate() {
+    game.dispatch({type: 'GAME_NEXT_FRAME', delta: 15})
+    setTimeout(animate, 1000)
+  }
+
+  animate()
+})(__GAME__)
